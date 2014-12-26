96f469d590cc8972d7ba390855bf500b73f3e781
spade <rintaro630@gmail.com>
2014-12-26 21:34:11 +0900

    authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,10 @@
 module SessionsHelper
-	def sign_in(user)
+    def sign_in(user)
     	remember_token = User.new_remember_token
     	cookies.permanent[:remember_token] = remember_token
     	user.update_attribute(:remember_token, User.encrypt(remember_token))
     	self.current_user = user
-  	end
+    end
 
   	def signed_in?
     	!current_user.nil?
@@ -19,8 +19,21 @@ module SessionsHelper
     	@current_user ||= User.find_by(remember_token: remember_token)
   	end
 
+    def current_user?(user)
+      user == current_user
+    end
+
     def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+    end
+
+    def store_location
+      session[:return_to] = request.url
     end
 end
